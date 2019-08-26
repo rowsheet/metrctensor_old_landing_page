c983e8838b6b9a895312d1299e1341ff73b93687
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2019-08-26 12:51:26 -0600

    Making base landing page fake_webserver demo conainer.

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# Fake Webserver 
-
-Fake webserver service for Docker Swarm Demo.
-
-[Github: rowsheet/fake_webserver](https://github.com/rowsheet/fake_webserver)
-
-[Dockerhub: rowsheet/fake_webserver](https://hub.docker.com/r/rowsheet/fake_webserver)
-
-### by: RowSheet 
+# Old MetrcTensor Landing Page. 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-REPO_NAME=fake_ecommerce
+REPO_NAME=old_metrctensor_landing
 SERVICE_NAME=dev--$REPO_NAME
 echo REPO_NAME:$REPO_NAME
 echo SERVICE_NAME:$SERVICE_NAME
@@ -19,9 +19,6 @@ source ./creds
 
 docker service create \
     --env PORT="$PORT" \
-    --env RATE_LIMITER="$RATE_LIMITER" \
-    --env SITE_NAME="$SITE_NAME" \
-    --env THEME="$THEME" \
-    --publish 8006:80 \
+    --publish 8007:80 \
     --name $SERVICE_NAME \
     rowsheet/$REPO_NAME:dev
